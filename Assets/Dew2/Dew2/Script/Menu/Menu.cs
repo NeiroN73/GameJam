bc2c236f6c59bfcdf814ca3253dcf600ffc09a7b
kreiz24 <73542256+kreiz24@users.noreply.github.com>
2023-01-11 19:21:03 +0300

    меню в конва не до конца

--- Assets/Dew2/Dew2/Script/Menu/Menu.cs
+++ Assets/Dew2/Dew2/Script/Menu/Menu.cs
@@ -4,15 +4,65 @@ using UnityEngine;
 
 public class Menu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject setings;
+    public GameObject menus;
+
+    public float timer;
+    public bool ispuse; 
+    public bool guipuse;
+
+    private void Awake()
     {
-        
+        Ñontinue();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Time.timeScale = timer; 
+        if (Input.GetKeyDown(KeyCode.Escape) && ispuse == false) 
+        { 
+            ispuse = true;
+            Cursor.visible = true;
+            menus.SetActive(true);
+        } 
+        else if (Input.GetKeyDown(KeyCode.Escape) && ispuse == true) 
+        {
+            Ñontinue();
+        }
+        if (ispuse == true) 
+        {
+            timer = 0; 
+            guipuse = true; 
+        }
+        else if (ispuse == false)
+        {
+            timer = 1f; guipuse = false;  
+        } 
+    } 
+     
+    public void Ñontinue()
+    {
+        ispuse = false;
+        Cursor.visible = false;
+        menus.SetActive(false);
+        setings.SetActive(false);
     }
+    public void Seting()
+    {
+        setings.SetActive(true);
+
+    }
+    public void Colsed()
+    {
+        setings.SetActive(false);
+    }
+    public void Restart()
+    {
+
+    }
+    public void Exed()
+    {
+
+    }
+
 }
